59e0471f6fa4e8015ce6fc6493e82fd75b952d08
haessler <lincetvm16@gmail.com>
2021-04-01 13:56:05 -0400

    add: cors and types

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const config = {
-  MongoDB:
+	port: '3600',
+	isDev: process.env.NODE_ENV !== 'production',
+	MongoDB:
 		process.env.NODE_ENV === 'production'
-		  ? process.env.MONGODB_STRING
-		  : 'mongodb://localhost:27017/zumetricsdb'
-}
+			? process.env.MONGODB_STRING
+			: 'mongodb://localhost:27017/zumetricsdb',
+};
 
-module.exports = config
+module.exports = config;
--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-'use strict'
+'use strict';
 
-const { makeExecutableSchema } = require('graphql-tools')
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const { readFileSync } = require('fs')
-const { join } = require('path')
-const resolvers = require('./lib/resolvers')
+const { makeExecutableSchema } = require('graphql-tools');
+const express = require('express');
+const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./lib/resolvers');
+const { isDev, port } = require('./config');
 
-const app = express()
-const port = '3600'
+const app = express();
 
 // Definiendo el Schema
 const typeDefs = readFileSync(
-  join(__dirname, 'lib', 'schema.graphql'),
-  'utf-8'
-)
+	join(__dirname, 'lib', 'schema.graphql'),
+	'utf-8',
+);
 
-const schema = makeExecutableSchema({ typeDefs, resolvers })
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Config Cors
+const whitelist = [
+	'http://localhost:3000',
+	'http://localhost:3333',
+	'http://localhost:3600',
+	'https://my.zumetrics.co',
+	'https://my.zumetrics.vercel.app',
+	'https://api-zumetrics.heroku.com',
+];
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+};
+
+app.use(cors(corsOptions));
 
 // Configurar la ruta
 
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
-  })
-)
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		rootValue: resolvers,
+		graphiql: isDev,
+	}),
+);
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/graphql`)
-})
+	console.log(`http://localhost:${port}/graphql`);
+});
